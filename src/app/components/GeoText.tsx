99ccddcdfbf1e989946205b82b0811cf22db1f50
senna-lang <asrsena3302@gmail.com>
2024-01-18 22:04:02 +0900

    position調整

--- src/app/components/GeoText.tsx
+++ src/app/components/GeoText.tsx
@@ -1,47 +1,51 @@
-'use client'
+'use client';
 import { Text } from '@react-three/drei';
 import { config, useSpring, animated } from '@react-spring/three';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { Mesh } from 'three';
 import { useAppContext } from '@/context/AppContext';
+import { Vector3 } from 'three';
+import { useFrame } from '@react-three/fiber';
+
+const Rig = ({ v = new Vector3() }) => {
+  return useFrame(state => {
+    state.camera.position.lerp(
+      v.set(state.mouse.x / 6, state.mouse.y / 6, 10),
+      0.05
+    );
+  });
+};
 
 const Texts = (props: any) => {
-  const { isTransition, setIsTransition } = useAppContext();
-  const ref = useRef<Mesh>(null!);
-
-  const { scale } = useSpring({
-    scale: isTransition ? 1.2 : 1,
-    config: config.wobbly,
-  });
-
   return (
-    <animated.mesh
-      ref={ref}
-      onClick={() => setIsTransition(!isTransition)}
-      scale={scale}
-    >
-      <Text
-        {...props}
-        fontSize={1.2}
-        font="/public/robots.txt"
-        color={'white'}
-        letterSpacing={0.02}
-      >
-        {props.text}
-      </Text>
-    </animated.mesh>
+    <Text {...props} font="/robots.txt" color={'white'} letterSpacing={0.02}>
+      {props.text}
+    </Text>
   );
 };
 
 const GeoText = () => {
+  const ref = useRef<Mesh>(null!);
+  const { isTransition, setIsTransition } = useAppContext();
+  const { scale } = useSpring({
+    scale: isTransition ? 1.05 : 1,
+    config: config.wobbly,
+  });
   return (
     <>
-      <Texts position={[-7, 4.5, 1]} text={'Sena Inomata'} />
-      <Texts position={[-8.4, 3, 1]} text={'Portfolio'} />
-      {/* <Texts position={[-7, 3.5, 1]} text={'Profile'} /> */}
-      {/* <Texts position={[-7, 1.5, 1]} text={'Works'} />
-      <Texts position={[-7, -0.5, 1]} text={'Skill set'} />
-      <Texts position={[-7, -2.5, 1]} text={'Git Hub'} /> */}
+      <Rig />
+      <animated.mesh ref={ref} scale={scale}>
+        <Texts
+          position={[-4, 6.9, 0]}
+          text={'Profile'}
+          fontSize={0.35}
+        />
+      </animated.mesh>
+      <Texts position={[-1, 6.9, 0]} text={'Works'} fontSize={0.35} />
+      <Texts position={[2, 6.9, 0]} text={'Skill set'} fontSize={0.35} />
+      <Texts position={[5, 6.9, 0]} text={'Contact'} fontSize={0.35} />
+      <Texts position={[-1.3, -1.9, 1]} text={'Sena Inomata'} fontSize={1.2} />
+      <Texts position={[-2.7, -3.3, 1]} text={'Portfolio'} fontSize={1.2} />
     </>
   );
 };
