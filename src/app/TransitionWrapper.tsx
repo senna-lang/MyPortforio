fef1c5e965ac4beddb09d7b6855aba2f706c3e82
senna-lang <asrsena3302@gmail.com>
2024-01-19 23:32:12 +0900

    framer-motionのwrapper

---
+++ src/app/TransitionWrapper.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { usePathname } from 'next/navigation';
+import { AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
+
+export default function TransitionWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathName = usePathname();
+
+  return (
+    // アンマウント時の動きをつけるために必要な記述
+    <AnimatePresence mode="wait" initial={false}>
+      <motion.div
+        key={pathName}
+        initial={{ opacity: 0 }} //初期状態
+        animate={{
+          opacity: 1,
+          transition: {
+            duration: 3,
+            // ease: [0.9, 0, 0.13, 1],
+          },
+        }} // マウント時
+        exit={{ opacity: 0 }} // アンマウント時
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+}
