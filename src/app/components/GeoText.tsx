81f476ff3c7cc456a9a72e99e0306c6038d18e32
senna-lang <asrsena3302@gmail.com>
2024-01-19 23:33:20 +0900

    テキストにonClickイベントで遷移

--- src/app/components/GeoText.tsx
+++ src/app/components/GeoText.tsx
@@ -1,13 +1,14 @@
 'use client';
 import { Text } from '@react-three/drei';
-import { config, useSpring, animated } from '@react-spring/three';
+import { animated } from '@react-spring/three';
 import { useRef } from 'react';
 import { Mesh } from 'three';
 import { useAppContext } from '@/context/AppContext';
 import { Vector3 } from 'three';
 import { useFrame } from '@react-three/fiber';
+import { useRouter } from 'next/navigation';
 
-const Rig = ({ v = new Vector3() }) => {
+export const Rig = ({ v = new Vector3() }) => {
   return useFrame(state => {
     state.camera.position.lerp(
       v.set(state.mouse.x / 6, state.mouse.y / 6, 10),
@@ -16,7 +17,7 @@ const Rig = ({ v = new Vector3() }) => {
   });
 };
 
-const Texts = (props: any) => {
+export const Texts = (props: any) => {
   return (
     <Text {...props} font="/robots.txt" color={'white'} letterSpacing={0.02}>
       {props.text}
@@ -26,24 +27,41 @@ const Texts = (props: any) => {
 
 const GeoText = () => {
   const ref = useRef<Mesh>(null!);
+  const router = useRouter();
   const { isTransition, setIsTransition } = useAppContext();
-  const { scale } = useSpring({
-    scale: isTransition ? 1.05 : 1,
-    config: config.wobbly,
-  });
+  // const { scale } = useSpring({
+  //   scale: isTransition ? 1.05 : 1,
+  //   config: config.wobbly,
+  // });
   return (
     <>
       <Rig />
-      <animated.mesh ref={ref} scale={scale}>
+      <animated.mesh ref={ref}>
         <Texts
           position={[-4, 6.9, 0]}
           text={'Profile'}
           fontSize={0.35}
+          onClick={() => router.push('/profile')}
         />
       </animated.mesh>
-      <Texts position={[-1, 6.9, 0]} text={'Works'} fontSize={0.35} />
-      <Texts position={[2, 6.9, 0]} text={'Skill set'} fontSize={0.35} />
-      <Texts position={[5, 6.9, 0]} text={'Contact'} fontSize={0.35} />
+      <Texts
+        position={[-1, 6.9, 0]}
+        text={'Works'}
+        fontSize={0.35}
+        onClick={() => router.push('/works')}
+      />
+      <Texts
+        position={[2, 6.9, 0]}
+        text={'Skill set'}
+        fontSize={0.35}
+        onClick={() => router.push('/skill')}
+      />
+      <Texts
+        position={[5, 6.9, 0]}
+        text={'Contact'}
+        fontSize={0.35}
+        onClick={() => router.push('/contact')}
+      />
       <Texts position={[-1.3, -1.9, 1]} text={'Sena Inomata'} fontSize={1.2} />
       <Texts position={[-2.7, -3.3, 1]} text={'Portfolio'} fontSize={1.2} />
     </>
