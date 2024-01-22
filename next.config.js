83e6581316edb0d176171e10ef61540f995a6eed
senna-lang <asrsena3302@gmail.com>
2024-01-22 19:21:10 +0900

    svgr導入に伴う変更

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   transpilePackages: ['three'],
-   images: {
-      domains: ['placehold.jp'],
+  transpilePackages: ['three'],
+  images: {
+    domains: ['placehold.jp'],
+    disableStaticImages: true, 
   },
-}
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+        },
+      ],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
