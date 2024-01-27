6c9574167632c46e6eb2086670b4ccef8b8f8acb
senna-lang <asrsena3302@gmail.com>
2024-01-27 16:58:10 +0900

    contact page 完成

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 
@@ -11,12 +12,27 @@ export default function Contact() {
           <div className=" mb-10">
             <h1 className=" text-5xl">Contact</h1>
           </div>
-          <div>
-            <ul>
-              <li className=" mb-1">e-mail:</li>
-              <li className=" mb-1">Green:</li>
-              <li className="mb-1">Wantedly:</li>
-            </ul>
+          <div className=" flex gap-6 mb-1">
+            <h2>email : </h2>
+            <p>asrsena@outlook.jp</p>
+          </div>
+          <div className=" flex gap-6 mb-1">
+            <h2>Wantedly : </h2>
+            <Link
+              href="https://www.wantedly.com/id/sena_inomata_ss"
+              target="_blank"
+            >
+              https://www.wantedly.com/id/sena_inomata_ss
+            </Link>
+          </div>
+          <div className=" flex gap-6 mb-1">
+            <h2>Green : </h2>
+            <Link
+              href="https://www.green-japan.com/profiles/show"
+              target="_blank"
+            >
+              https://www.green-japan.com/profiles/show
+            </Link>
           </div>
         </div>
       </div>
