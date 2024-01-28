6d04e616cd6e035204d26ec3eb6ff271142c5de6
senna-lang <asrsena3302@gmail.com>
2024-01-28 21:32:00 +0900

    ハンバーガーメニュー実装

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,25 +1,70 @@
+"use client";
 import Link from "next/link";
 import { ScaleUp } from "./animations/ScaleUp";
+import { useDisclosure } from "@mantine/hooks";
+import { Burger, Drawer } from "@mantine/core";
 
 const Header = () => {
+  const [opened, { toggle }] = useDisclosure();
   return (
-    <div className=" hidden h-6 w-auto p-10 lg:block">
-      <nav>
+    <div className="h-6 w-auto p-10">
+      <nav className=" hidden md:block">
         <ul className=" flex cursor-pointer justify-center gap-20 text-2xl font-thin tracking-wide text-slate-300">
-          <Link href="/profile">
-            <ScaleUp text="Profile" />
-          </Link>
-          <Link href="/works">
-            <ScaleUp text="Works" />
-          </Link>
-          <Link href="/skill">
-            <ScaleUp text="Skill set" />
-          </Link>
-          <Link href="/contact">
-            <ScaleUp text="Contact" />
-          </Link>
+          <li>
+            <Link href="/profile">
+              <ScaleUp text="Profile" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/works">
+              <ScaleUp text="Works" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/skill">
+              <ScaleUp text="Skill set" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <ScaleUp text="Contact" />
+            </Link>
+          </li>
         </ul>
       </nav>
+      <Burger
+        opened={opened}
+        onClick={toggle}
+        aria-label="Toggle navigation"
+        color="white"
+        className="md:hidden"
+      />
+      <Drawer opened={opened} onClose={toggle}>
+        <div className="px-4 pt-[78px] font-bold">
+          <ul className="flex cursor-pointer flex-col gap-2 text-2xl font-thin tracking-wide ">
+            <li>
+              <Link href="/profile" onClick={toggle}>
+                Profile
+              </Link>
+            </li>
+            <li>
+              <Link href="/works" onClick={toggle}>
+                Works
+              </Link>
+            </li>
+            <li>
+              <Link href="/skill" onClick={toggle}>
+                Skill set
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" onClick={toggle}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </Drawer>
     </div>
   );
 };
